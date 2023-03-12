72adc6a46cf351929efa598889e6e8d445ddffe5
Priya Kushawaha <99608708+Kushpriya@users.noreply.github.com>
2023-03-12 09:47:53 +0545

    java

---
+++ Inheritance/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+        int height = 30;
+        String color = "Red";
+        void getHeight() {
+
+                System.out.println("Height of animal " + height);
+        }
+        void getColor()
+        {
+                System.out.println("Animal color " + color);
+        }
+}
